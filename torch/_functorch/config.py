15e33630397f81bf86b63b784e57ea69a2675093
Han Qi <qihan@fb.com>
2023-03-09 14:02:02 -0800

    Replace _dynamo.config with an object instead of module
    Current usage patterns of setting and reading fields on config will work
    unchanged.
    
    Only changes needed going forward:
    
    1. Files inside of _dynamo folder need to access config via
       from torch._dynamo.config_util import config instead of
       from torch._dynamo import config. Because _dynamo/__init__.py
       imports some of the files so it would be circular import.
    
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    reset test files

--- torch/_functorch/config.py
+++ torch/_functorch/config.py
@@ -10,33 +10,27 @@ Global flags for aot autograd
 import os
 import sys
 
-use_functionalize = True
+use_functionalize: bool = True
 
-use_fake_tensor = True
+use_fake_tensor: bool = True
 
 # can be useful for debugging if we are incorrectly creating meta fake tensors
-fake_tensor_allow_meta = os.environ.get("FAKE_ALLOW_META", True)
+fake_tensor_allow_meta: bool = os.environ.get("FAKE_ALLOW_META", True)
 
 # Enables optional asserts in hotpath code to check for errors.  If
 # you are seeing weird accuracy problems, try turning this on.
 # This is currently off by default as it will harm tracing time,
 # but it is on by default for aot_eager.
-debug_assert = False
+debug_assert: bool = False
 
-debug_fake_cross_ref = os.environ.get("AOT_FAKE_CROSSREF", False)
+debug_fake_cross_ref: bool = os.environ.get("AOT_FAKE_CROSSREF", False)
 
-debug_partitioner = os.environ.get("AOT_PARTITIONER_DEBUG", False)
+debug_partitioner: bool = os.environ.get("AOT_PARTITIONER_DEBUG", False)
 
-static_weight_shapes = True
+static_weight_shapes: bool = True
 
 # Applies CSE to the graph before partitioning
-cse = True
+cse: bool = True
 
 # Restricts the amount of computation AOTAutograd can do.
-max_dist_from_bw = 3
-
-
-from .._dynamo.config_utils import install_config_module
-
-# adds patch, save_config, invalid config checks, etc
-install_config_module(sys.modules[__name__])
+max_dist_from_bw: bool = 3
